a6a3d2bd806463ea067f37b1bfb3eff8bd9d41ad
vishnuabntechcorp <vishnu@abntechcorp.com>
2024-08-07 21:19:35 +0530

    Update ebs.tf

--- ebs.tf
+++ ebs.tf
@@ -1,5 +1,5 @@
 resource "aws_ebs_volume" "example" {
-  availability_zone = "ap-south-1a"
+  availability_zone = "ap-south-1a"   # add the availability_zone  go to vpc and flow map the can see  availability_zone
   size              = 6
 
   tags = {
