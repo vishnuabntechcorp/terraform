91a617e8abb42a548818a978ca6b2f924151545c
vishnuabntechcorp <vishnu@abntechcorp.com>
2024-08-14 19:10:18 +0530

    Update main.tf
    main

--- cloudwatch log group/main.tf
+++ cloudwatch log group/main.tf
@@ -8,6 +8,6 @@ resource "aws_cloudwatch_log_group" "log_group" {
 }
 
 resource "aws_cloudwatch_log_stream" "log_stream" {
-  name           = "devops-log-stream"                          // give the name 
+  name           = "devops-log-stream"                          
   log_group_name = aws_cloudwatch_log_group.log_group.name
 }
