d02955e5d7b52dab2c4f80b676145e9a1bbeebaa
vishnuabntechcorp <vishnu@abntechcorp.com>
2024-08-20 15:46:47 +0530

    Create  main.tf

---
+++ S3lifecycle & polices/lifecycle and bucket/main.tf
@@ -0,0 +1,40 @@
+resource "aws_s3_bucket" "create-s3-bucket" {
+bucket = "${var.bucket-name}"
+ acl = "private"
+
+  lifecycle_rule {
+    id = "archive"
+    enabled = true
+    transition {
+      days = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Enviroment: "QA"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_metric" "enable-metrics-bucket" {
+  bucket = "${var.bucket-name}"
+  name   = "EntireBucket"
+}
