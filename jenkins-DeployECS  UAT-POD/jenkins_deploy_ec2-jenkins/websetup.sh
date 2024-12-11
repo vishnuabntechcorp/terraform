d6859b88f86c2a8f99b8cc1729d6974e903f16f0
vishnuabntechcorp <vishnu@cloudscore.ai>
2024-12-11 19:05:59 +0530

    jenkins ec2 deployment

---
+++ jenkins-DeployECS  UAT-POD/jenkins_deploy_ec2-jenkins/websetup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Installing Dependencies
+echo "########################################"
+echo "Installing packages."
+echo "########################################"
+sudo apt update -y > /dev/null
+sudo apt install wget unzip apache2 -y > /dev/null
+echo
+
+# Creating Temp Directory
+echo "########################################"
+echo "Starting Artifact Deployment"
+echo "########################################"
+mkdir -p /tmp/webfiles
+cd /tmp/webfiles
+echo
+
+wget https://www.tooplate.com/zip-templates/2130_waso_strategy.zip > /dev/null
+unzip 2130_waso_strategy.zip > /dev/null
+sudo cp -r 2130_waso_strategy/* /var/www/html/
+echo
+
+# Bounce Service
+echo "########################################"
+echo "Restarting Apache Webserver service"
+echo "########################################"
+sudo systemctl restart apache2
+echo
+
+# Clean Up
+echo "########################################"
+echo "Removing Temporary Files"
+echo "########################################"
+sudo rm -rf /tmp/webfiles
+echo
